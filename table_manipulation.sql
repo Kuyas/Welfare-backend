93534ed3e746d4ff257eaf07195271dc87281b5c
Shreyas Sunil Kulkarni <shreyask2903@gmail.com>
2018-06-24 11:45:23 +0530

    declaration, manipulation sql scripts for user db

---
+++ table_manipulation.sql
@@ -0,0 +1,9 @@
+
+-- Dumping data for table `users`
+--
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `created_at`, `updated_at`) VALUES
+(1, 'shrey', '08fa0a7e19b1eaaa7655d54fabf8ec61', 's@gmail.com', '2018-06-09 00:00:00', '2018-06-10 00:00:00');
+
+
+--
+-- Indexes for dumped tables
