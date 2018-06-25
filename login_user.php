6a3c16fa97ae3acb89df6f4177c48ad7d4f9cdc2
Shreyas Sunil Kulkarni <shreyask2903@gmail.com>
2018-06-25 14:40:05 +0530

    Added Login - Working version and safer version

--- login_user.php
+++ login_user.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/db_config.php';
 
 
 /**
- * This php file
+ * This php file is for logging in
  * 
  */
  //array for JSON response
@@ -17,27 +17,37 @@ require_once __DIR__ . '/db_config.php';
     $password = $_POST['password'];
     $password = md5($password);
 
+    $mobile_number = stripslashes($mobile_number);
+    $password = stripslashes($password);
+ 
+
+
 
 
    //connecting to db
     $database = DB_DATABASE;
     $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_DATABASE) or die(mysql_error());
-    $query = "SELECT `user_mobile`, `user_password` FROM `user` WHERE `user_mobile` = '.$mobile_number.' AND `user_password` = '.$password.'";
+    $mobile_number = mysqli_real_escape_string($con, $mobile_number);
+    $password = mysqli_real_escape_string($con, $password);
     
+    $query = "SELECT * FROM user WHERE user_mobile = '". $mobile_number ."' AND user_password = '". $password."'" ;
     $result = mysqli_query($con, $query);
   
 
-    if($result){
+    if(mysqli_num_rows($result) > 0){
         //Login Successfully
+        $result = mysqli_fetch_array($result);
         $response["success"] = 1;
+        $response["id"] = $result[0];
+       
         echo json_encode($response);
     }else{
 
         //Login Failed
-        $response["result"] = $result;
+        // $response["result"] = $result;
         $response["success"] = 0;
-        $response["message"] = $mobile_number;
-        $response["messag1"] = $password;
+        // $response["message"] = $mobile_number;
+        // $response["messag1"] = $password;
         echo json_encode($response);
     }
 
