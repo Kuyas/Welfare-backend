2078aa0f8701ef4c58ec23f5325a79cdfa35ffad
Omkar Kanade <omukanade@gmail.com>
2018-06-27 12:12:00 +0530

    regex and sanitization added to all forms

--- register_user.php
+++ register_user.php
@@ -13,7 +13,8 @@ require_once __DIR__ . '/db_config.php';
  //check for required fields
  if(isset($_POST['mobile_number']) && isset($_POST['password'])){
 
-    if () {
+    if (!preg_match("^[0-9]{10}$", $_POST['mobile_number'])
+        !preg_match("^[a-zA-Z0-9]{8,16}$", $_POST['password'])) {
 
         // input does not match the corresponding given data types
         $response["success"] = -2;
@@ -34,7 +35,7 @@ require_once __DIR__ . '/db_config.php';
 
         $mobile_number = mysqli_real_escape_string($con, $mobile_number);
         $password = mysqli_real_escape_string($con, $password);
-        
+
         $query = "INSERT INTO user(user_mobile, user_password, user_date_created, user_date_modified) VALUES ('$mobile_number','$password', NOW(), NOW())";
         $result = mysqli_query($con, $query);
 
