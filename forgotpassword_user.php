e63b940cf5e79209c643ab746f15acc9a843c624
twitu <bhanuka.ishan@gmail.com>
2018-06-28 13:47:29 +0530

    forgot and signup php activities separate userid retrieval php

--- forgotpassword_user.php
+++ forgotpassword_user.php
@@ -12,29 +12,37 @@ require_once __DIR__ . '/db_config.php';
  //array for JSON response
  $response = array();
 
- if(isset($_POST['mobile_number']) && isset($_POST['password'])){   
-    $mobile_number = $_POST['mobile_number'];
+ if(isset($_POST['user_id']) && isset($_POST['password'])){   
+    $user_id = $_POST['user_id'];
     $password = $_POST['password'];
-    $password = md5($password);
+    
+    if (!preg_match("~^[a-zA-Z0-9]{8,16}$~", $_POST['password'])) {
 
-
-   //connecting to db
-    $database = DB_DATABASE;
-    $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_DATABASE) or die(mysql_error());
-    $query = "UPDATE USER SET `USER_PASSWORD`='".$password."', `USER_DATE_MODIFIED`=NOW() WHERE `USER_MOBILE`='".$mobile_number."'";
-
-    $result = mysqli_query($con, $query);
-  
-
-    if($result){
-        //Login Successfully
-        $response["response_code"] = 1;
+        // input does not match the corresponding given data types
+        $response["response_code"] = -2;
         echo json_encode($response);
-    }else{
+    } else {
+        $password = md5($password);
 
-        //Login Failed
-        $response["response_code"] = 0;
-        echo json_encode($response);
+
+       //connecting to db
+        $database = DB_DATABASE;
+        $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_DATABASE) or die(mysql_error());
+        $query = "UPDATE USER SET USER_PASSWORD = '".$password."' WHERE USER_ID = '".$user_id."'";
+
+        $result = mysqli_query($con, $query);
+      
+
+        if($result){
+            //Login Successfully
+            $response["response_code"] = 1;
+            echo json_encode($response);
+        }else{
+
+            //Login Failed
+            $response["response_code"] = 0;
+            echo json_encode($response);
+        }
     }
 
  }else{
